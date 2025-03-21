874dbe1f770fb80140d5520102cb3c78498c9b10
Harrie <hbessner@googlemail.com>
2025-03-21 10:11:06 +0100

    fix: Use palette after field's definition now.

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -2,7 +2,6 @@
 
 use Itsbessner\ConditionalContainer\Elements\ConditionalContainer;
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['itsbessner_conditional_container'] = '{type_legend},itsbessner_conditional_selection,itsbessner_conditional_days_before';
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['itsbessner_conditional_selection'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['itsbessner_conditional_selection'],
@@ -20,3 +19,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['itsbessner_conditional_days_before']
     'eval' => ['rgxp' => 'natural', 'tl_class' => 'w50'],
     'sql' => "int(10) unsigned NOT NULL default '0'",
 ];
+
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['itsbessner_conditional_container'] = '{type_legend},itsbessner_conditional_selection,itsbessner_conditional_days_before';
